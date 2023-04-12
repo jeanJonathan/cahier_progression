e0bb1c406b4b0857ba3006a393d1dae5b5fa2404
Jean Jonathan <jeanjonathank2@gmail.com>
2023-04-12 14:00:16 +0200

    Mise a jour des dependencce et resolution des probleme de d incompatibilite

--- lib/main.dart
+++ lib/main.dart
@@ -24,7 +24,7 @@ class MyApp extends StatelessWidget {
         // is not restarted.
         primarySwatch: Colors.blue,
       ),
-      home: const MyHomePage(title: 'Home Page pour le cahier progression'),
+      home: const MyHomePage(title: 'Home Page pour le cahier de progression'),
     );
   }
 }
